8c5c6c2111bf38e729e63bbcbe2cb86bc04c556c
mkb <mk9007@163.com>
2025-09-24 01:56:44 +0800

    chore/update [branch: master] 2025-09-24 01:56

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,20 @@
-declare const ton: any;
+type UniType = typeof uni;
+type WxType = typeof wx;
+type MergeApi<U, W> = {
+    [K in keyof U | keyof W]: K extends keyof U ? U[K] : K extends keyof W ? W[K] : never;
+};
+type BaseApiType = MergeApi<UniType, WxType>;
+type Promisify<T> = {
+    [K in keyof T]: T[K] extends (...args: infer A) => any ? A[0] extends {
+        success?: any;
+        fail?: any;
+    } ? (options?: Omit<A[0], 'success' | 'fail'> & {
+        $async?: boolean;
+    }, ...args: A extends [any, ...infer R] ? R : []) => Promise<any> : T[K] : T[K];
+};
+type PluginApi = Record<string, (...args: any[]) => any>;
+type TonType = Promisify<BaseApiType> & {
+    use: (plugin: PluginApi) => void;
+};
+declare const ton: TonType;
 export default ton;
--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,20 @@
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
 // 插件仓库
 const customApiStore = {};
+// 注册插件
 function use(plugin) {
     Object.assign(customApiStore, plugin);
 }
-
-const baseApi =
-    typeof uni !== "undefined"
-        ? uni
-        : typeof wx !== "undefined"
-            ? wx
-            : {};
-
 // 常用异步 API 列表
 const asyncApis = new Set([
     // 网络
@@ -41,47 +45,42 @@ const asyncApis = new Set([
     "startPullDownRefresh", "stopPullDownRefresh",
     "vibrateShort", "vibrateLong", "scanCode"
 ]);
-
+// 运行时代理
+const baseApi = typeof uni !== 'undefined' ? uni :
+    (typeof wx !== 'undefined' ? wx : {});
 const ton = new Proxy({ use }, {
     get(target, prop) {
-        if (typeof prop === "symbol") return target[prop];
-        if (prop in target) return target[prop];
-
+        if (typeof prop === 'symbol')
+            return target[prop];
+        if (prop in target)
+            return target[prop];
         // 插件方法优先
         if (prop in customApiStore) {
             return customApiStore[prop];
         }
-
         const fn = baseApi[prop];
-        if (typeof fn !== "function") return undefined;
-
-        // 判断是否异步 API
+        if (typeof fn !== 'function')
+            return undefined;
         const isAsync = asyncApis.has(prop);
-
         if (!isAsync) {
             // 同步 API → 直接透传
             target[prop] = (...args) => fn(...args);
-        } else {
-            const forcedAsyncFn = (options = {}, ...args) => {
-                const { $async, ...restOptions } = options || {};
+        }
+        else {
+            // 异步 API → 支持 $async 参数
+            target[prop] = (options = {}, ...args) => {
+                const _a = options || {}, { $async } = _a, restOptions = __rest(_a, ["$async"]);
+                if ($async === false) {
+                    // 回调风格
+                    return fn(restOptions, ...args);
+                }
+                // Promise 风格
                 return new Promise((resolve, reject) => {
-                    fn(
-                        {
-                            ...restOptions,
-                            success: (res) => resolve(res),
-                            fail: (err) => reject(err),
-                        },
-                        ...args
-                    );
+                    fn(Object.assign(Object.assign({}, restOptions), { success: (res) => resolve(res), fail: (err) => reject(err) }), ...args);
                 });
             };
-
-            // target[prop] 统一函数：根据 $async 决定
-            target[prop] = forcedAsyncFn;
         }
-
         return target[prop];
     }
 });
-
 export default ton;
